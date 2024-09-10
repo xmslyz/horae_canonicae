a2c87f081d3eed80df181acdeaf291a39865b77e
xmslyz <xmslyz@gmail.com>
2024-09-10 16:01:25 -0500

    all test passed in creator.py

--- creator.py
+++ creator.py
@@ -115,99 +115,171 @@ def easter_time(year):
     return ash_wednesday, easter, pentecost
 
 
-def advent_season(target_date: datetime.date, lit_year: int):
-    start_date = advent_start_date(lit_year)
-    end_date = datetime.datetime.strptime(
-        f'24.12.{lit_year}', '%d.%m.%Y').date()
-    return start_date <= target_date <= end_date
-
-
-def xmass_season(target_date: datetime.date, lit_year: int):
-    start_date = datetime.datetime.strptime(
-        f'25.12.{lit_year}', '%d.%m.%Y').date()
-    end_date = baptism_sunday(lit_year + 1)
-    return start_date <= target_date <= end_date
-
-
-def ordinary_season(target_date, lit_year: int):
-    first_part_start = baptism_sunday(lit_year + 1)
-    first_part_end, _, second_part_start = easter_time(lit_year + 1)
-
-    first_part = first_part_start < target_date < first_part_end
-    second_part = second_part_start < target_date < advent_start_date(
-        lit_year + 1)
-
-    return bool(first_part | second_part)
-
-
-def lent_seson(target_date, lit_year: int):
-    start_date, end_date, _ = easter_time(lit_year + 1)
-    return start_date <= target_date <= end_date
-
-
-def easter_seson(target_date, lit_year: int):
-    _, start_date, end_date = easter_time(lit_year + 1)
-    return start_date <= target_date <= end_date
-
-
-def find_proper_scope(search_date):
-    # Szukamy przedziału Adwentu, w którym znajduje się data
-    search_year = search_date.year
-
-    # Sprawdzamy lata do przodu i wstecz, w którym przedziale znajduje się data
-    for s_year in range(search_year - 1, search_year + 2):
-        advent = advent_start_date(s_year)
-        next_advent = advent_start_date(s_year + 1)
-
-        if advent <= search_date < next_advent:
-            return s_year
-
-    return None  # Jeśli nie znaleziono, co teoretycznie nie powinno się zdarzyć
-
-
-def check_date(year, month, day):
-    target = datetime.date(year, month, day)
-    cal_year = find_proper_scope(target)
-
-    print("adv", advent_season(target, cal_year))
-    print("xma", xmass_season(target, cal_year))
-    print("ord", ordinary_season(target, cal_year))
-    print("len", lent_seson(target, cal_year))
-    print("eas", easter_seson(target, cal_year))
-
-
-def find_proper_week(initium, finis, year, c_day) -> int:
+def get_liturgical_season(target_date: datetime.date) -> str:
     """
+    Determines the liturgical season for a given date
+
+    Parameters:
+        target_date (datetime.date): The date for which the liturgical season is
+         determined.
+        lit_year (int): The liturgical year in which to check the date.
+
+    Returns:
+        str: The name of the liturgical season ('Advent', 'Christmas',
+        'Ordinary', 'Lent', 'Easter'), or 'Unknown' if the date does not fall
+        within any defined liturgical period.
+    """
+    lit_year = find_proper_scope(target_date)
+
+    def advent_season(target_date: datetime.date, lit_year: int) -> bool:
+        start_date = advent_start_date(lit_year)
+        end_date = datetime.datetime.strptime(f'24.12.{lit_year}',
+                                              '%d.%m.%Y').date()
+
+        return start_date <= target_date <= end_date
+
+    def xmass_season(target_date: datetime.date, lit_year: int) -> bool:
+        start_date = datetime.datetime.strptime(f'25.12.{lit_year}',
+                                                '%d.%m.%Y').date()
+        end_date = baptism_sunday(lit_year + 1)
+        return start_date <= target_date < end_date
+
+    def ordinary_season(target_date: datetime.date, lit_year: int) -> bool:
+        first_part_start = baptism_sunday(lit_year + 1)
+        first_part_end, _, second_part_start = easter_time(lit_year + 1)
+        first_part = first_part_start < target_date < first_part_end
+        second_part = second_part_start < target_date < advent_start_date(
+            lit_year + 1)
+        return first_part or second_part
+
+    def lent_season(target_date: datetime.date, lit_year: int) -> bool:
+        start_date, end_date, _ = easter_time(lit_year + 1)
+        return start_date <= target_date < end_date
+
+    def easter_season(target_date: datetime.date, lit_year: int) -> bool:
+        _, start_date, end_date = easter_time(lit_year + 1)
+        return start_date <= target_date <= end_date
+
+    # Map seasons to their corresponding functions
+    seasons = {
+        'Advent': lambda: advent_season(target_date, lit_year),
+        'Christmas': lambda: xmass_season(target_date, lit_year),
+        'Ordinary': lambda: ordinary_season(target_date, lit_year),
+        'Lent': lambda: lent_season(target_date, lit_year),
+        'Easter': lambda: easter_season(target_date, lit_year),
+    }
+
+    # Check which season the date belongs to
+    for season, check_func in seasons.items():
+        if check_func():
+            return season
+
+    return 'Unknown'
+
+
+def find_proper_scope(date_to_search: datetime.date) -> int:
+    """
+    Finds the year in which the new liturgical year starts.
+
     ...
 
+    The function determines which liturgical year the `date` falls into
+    by checking the Advent periods of the current year, and the next year.
+
+    Parameters:
+        date_to_search (datetime.date): The date for which the corresponding
+        liturgical year start year is sought.
+
+    Returns:
+        int: The year in which the new liturgical year (Advent) starts, which
+        includes the `date`.
     """
-    sundays = []
-    sun_week_no = []
+    try:
+        search_year = date_to_search.year
 
-    scope_part_one: datetime.timedelta = easter_time(year)[0] - baptism_sunday(year)
+        # Check the Advent period in the previous, current, and next year
+        for s_year in range(search_year - 1, search_year + 2):
+            advent = advent_start_date(s_year)
+            next_advent = advent_start_date(s_year + 1)
 
-    # szukaj niedziel w 1 części Okresu Zwykłego
-    for i in range(0, scope_part_one.days + 1):
-        q_day = baptism_sunday(year) + datetime.timedelta(i)
-        # if queried day is sunday, append to a list
-        if q_day.weekday() == 6:
-            sundays.append(q_day)
-            sun_week_no.append(q_day.isocalendar()[1])
+            if advent <= date_to_search < next_advent:
+                return s_year
 
-    scope_part_two: datetime.timedelta = (advent_start_date(year) - easter_time(
-        year)[2])
+    except Exception as e:
+        raise Exception(e)
 
-    for i in range(0, scope_part_two.days):
-        q_day = easter_time(year)[2] + datetime.timedelta(i)
-        if q_day.weekday() == 6:
-            sundays.append(q_day)
-            # - 13 weeks for lent and eastertide
-            sun_week_no.append(q_day.isocalendar()[1] - 13)
 
-    q_sunday = first_day_of_week(sundays, c_day)
-    for index in range(0, 5):
-        if q_sunday in sundays[index::4]:
-            return index + 1
+def find_proper_week(initium: datetime.date, finis: datetime.date,
+                     c_day: datetime.date) -> int:
+    """
+    Finds the index of the week (1 to 4)
+
+    ...
+
+    Where the given day (`c_day`) belongs within the period from `initium` to
+    `finis`.
+
+    Parameters:
+        initium (datetime.date): Start date of the period.
+        finis (datetime.date): End date of the period.
+        c_day (datetime.date): The date for which to find the corresponding
+        week.
+
+    Returns:
+        int: The index of the corresponding week (1-5),
+        or raises an exception if the date is invalid.
+
+    Raises:
+        ValueError: If the given dates are invalid or if `c_day` is not within
+        the range `[initium, finis]`.
+        IndexError: If the calculated weeks do not fit within the expected
+        range.
+    """
+    try:
+        if finis < initium:
+            raise ValueError(f'The end date is earlier than initial date.')
+        if (finis - initium).days > 193:
+            raise ValueError(
+                f'The span between {initium} and {finis} cannot exceed 193 days.')
+        if initium == finis == c_day:
+            raise ValueError(
+                f'The dates {initium}, {finis}, and {c_day} are all the same.')
+        if c_day < initium or c_day > finis:
+            raise ValueError(
+                f'The date {c_day} is not within the range {initium}-{finis}.')
+
+        sundays = []
+        sun_week_no = []
+        scope: datetime.timedelta = finis - initium
+
+        # Find all Sundays within the date range
+        for i in range(0, scope.days + 1):
+            q_day = initium + datetime.timedelta(i)
+            # If queried day is a Sunday, append it to the list
+            if q_day.weekday() == 6:
+                sundays.append(q_day)
+                sun_week_no.append(q_day.isocalendar()[1])
+
+        # Find the Sunday that corresponds to `c_day`
+        q_sunday = first_day_of_week(sundays, c_day)
+
+        # Find the index of the proper week
+        for index in range(4):
+            if q_sunday in sundays[index::4]:
+                return index + 1
+
+        # If no match is found, raise an error
+        raise IndexError(
+            "The calculated weeks do not fit within the expected range.")
+
+    except ValueError as ve:
+        raise ve  # Re-raise ValueError to be caught by the specific tests
+
+    except IndexError as ie:
+        raise ie  # Re-raise IndexError to be caught by the specific tests
+
+    except Exception as e:
+        raise Exception(f"An error occurred: {e}")
 
 
 def first_day_of_week(sundays: list, c_day: datetime.date) -> datetime.date:
@@ -222,17 +294,37 @@ def first_day_of_week(sundays: list, c_day: datetime.date) -> datetime.date:
     Returns:
         datetime.date: The Sunday that corresponds to the given day.
 
+    Raises:
+        ValueError: If no valid Sunday is found for the given day.
     """
-    for sunday in sundays:
-        # Calculate the difference in days
-        difference = (c_day - sunday).days
+    try:
+        for sunday in sundays:
+            # Calculate the difference in days
+            difference = (c_day - sunday).days
 
-        # If the difference is within the current week (0 to 6),
-        # return the Sunday
-        if 0 <= difference < 7:
-            return sunday
+            # If the difference is within the current week (0 to 6),
+            # return the Sunday
+            if 0 <= difference < 7:
+                return sunday
+
+        # If no valid Sunday is found, raise ValueError
+        raise ValueError(f"No valid Sunday found for the date {c_day}")
+
+    except ValueError as e:
+        print(f"Error: {e}")
+        raise  # Re-raise the ValueError to propagate the error
 
 
 if __name__ == "__main__":
-    s_day = datetime.date(2024, 9, 9)
-    print(find_proper_week(2024, s_day))
+    s_day = datetime.date(2024, 2, 12)
+    sundays = [
+        datetime.date(2023, 12, 24),  # Christmas
+        datetime.date(2023, 12, 31),  # End of 2023
+        datetime.date(2024, 1, 7),  # 1st week of 2024
+        datetime.date(2024, 1, 14),
+        datetime.date(2024, 1, 21),
+        datetime.date(2024, 1, 28),
+        datetime.date(2024, 2, 4)
+    ]
+
+    print(first_day_of_week(sundays, s_day))
