a8e8954d3b03065b154234b55eeeca8f26a95fed
xmslyz <xmslyz@gmail.com>
2024-09-14 14:34:46 -0500

    office of readings : ready

--- main_classes.py
+++ main_classes.py
@@ -4,6 +4,10 @@
 import abc
 import json
 from abc import ABC
+from datetime import datetime
+from colorama import Fore, Style, init
+
+from creator import Skeleton
 
 
 class Hours:
@@ -11,6 +15,15 @@ class Hours:
         self.__rank = None
         self.__joined = False
         self.__is_lent = False
+        self.__full = True
+        self.__clasic_td = True
+        self.calendar = self.calendar()
+
+    @staticmethod
+    def calendar():
+        sk = Skeleton(datetime.today().date())
+        return sk
+
 
     @abc.abstractmethod
     def pray(self):
@@ -28,6 +41,29 @@ class Hours:
     def psalmodia(self):
         """ """
         raise NotImplementedError
+
+    @abc.abstractmethod
+    def psalm(self, psalm):
+        """        """
+        ant = psalm["antifona"] + "\n"
+        ind = psalm["psalm_index"] + "\n" if len(psalm["psalm_index"]) > 0 else ""
+        tit = psalm["psalm_title"] + "\n" if len(psalm["psalm_title"]) > 0 else ""
+        com = psalm["psalm_comment"] + "\n" if len(psalm["psalm_comment"]) > 0 else ""
+        txt = psalm["psalm_txt"] + "\n"
+
+        w = psalm.get("werse", "")
+        werse = w if w != "" else ""
+
+        return (
+            Fore.RED + "Ant. " + Fore.GREEN + ant +
+            Fore.LIGHTRED_EX + ind +
+            Fore.LIGHTYELLOW_EX + tit +
+            Fore.LIGHTCYAN_EX + com +
+            Style.RESET_ALL + txt +
+            Fore.RED + "Ant. " + Fore.GREEN + ant +
+            Style.RESET_ALL + "\n" + werse
+        )
+
 
     @abc.abstractmethod
     def readings(self):
@@ -53,9 +89,9 @@ class Hours:
         return self.__rank
 
     @rank.setter
-    def rank(self, rank_type: str):
+    def rank(self, rank_type: str = "o"):
         """
-        s - solemnity, f - feast, m - memory obligatory, l - memory free, a - aditional memory
+        s - solemnity, f - feast, m - memory obligatory, l - memory free, a - aditional memory, o - ordinary
         """
         self.__rank = rank_type
 
@@ -85,18 +121,54 @@ class Hours:
         """ Should be True in lent season (no TeDeum and Aleluia) """
         self.__is_lent = solo
 
+    @property
+    def full(self):
+        return self.__full
+
+    @full.getter
+    def full(self):
+        return self.__full
+
+    @full.setter
+    def full(self, full_version: bool = False):
+        """ Should be True in lent season (no TeDeum and Aleluia) """
+        self.__full = full_version
+
+    @property
+    def clasic_td(self):
+        return self.__clasic_td
+
+    @clasic_td.getter
+    def clasic_td(self):
+        return self.__clasic_td
+
+    @clasic_td.setter
+    def clasic_td(self, is_clasic: bool = True):
+        """ True for clasic version of Te Deum, False for poetic """
+        self.__clasic_td = is_clasic
+
 
 class Readings(Hours, ABC):
     def __init__(self):
         super().__init__()
         self.const = self.get_const()
+        self.base = self.get_base()
 
     @staticmethod
     def get_const():
         with open("library/cons.json", encoding="utf-8") as f:
             return json.load(f)
 
+    @staticmethod
+    def get_base():
+        with open("base_files/ot/ot_lec.json", encoding="utf-8") as f:
+            return json.load(f)
+
     def pray(self):
+        print("\u2554" + "\u2550" * 17 + "\u2557")
+        print("\u272A GODZINA CZYTAŃ \u272A")
+        print("\u255A" + "\u2550" * 17 + "\u255D")
+
         print(self.opening())
         print(self.hymn())
         print(self.psalmodia())
@@ -108,13 +180,7 @@ class Readings(Hours, ABC):
 
     def opening(self):
         aleluya = " Alleluja." if not self.is_lent else ""
-
-        txt = (f"K. Bo\u017ce, wejrzyj ku wspomo\u017ceniu memu.\n"
-               f"W. Panie, po\u015bpiesz ku ratunkowi memu.\n\n"
-               f"Chwa\u0142a Ojcu i Synowi, i Duchowi \u015awi\u0119temu.\n"
-               f"Jak by\u0142a na pocz\u0105tku, teraz i zawsze,\ni na wieki wiek\u00f3w. Amen.{aleluya}\n")
-
-        return "" if self.joined else txt
+        return "" if self.joined else f"{self.const["opening"]}{aleluya}\n"
 
     def hymn(self):
         """
@@ -122,7 +188,11 @@ class Readings(Hours, ABC):
         s|f - propia | comunes
         m - comunes | psalter
         """
-        return "HYMN"
+        psalter_str = str(self.calendar.current_psalter_week)
+        day_str = str(self.calendar.lg_day.weekday())
+
+        return f"\u2731 HYMN \u2731\n{self.base[psalter_str][day_str]['hymn']}"
+
 
     def psalmodia(self):
         """
@@ -130,57 +200,71 @@ class Readings(Hours, ABC):
         s|f - propia
         m - psalter *propia
         """
-        return "PSALMODIA"
+        psalter_str = str(self.calendar.current_psalter_week)
+        day_str = str(self.calendar.lg_day.weekday())
+
+        print("\u2731 PSALMODIA \u2731\n")
+        psalmody = ''
+        for psalm in ["psalm1", "psalm2", "psalm3"]:
+            psalmody += self.psalm(self.base[psalter_str][day_str][psalm])
+
+        return psalmody
+
+
 
     def werse(self):
         """
         s|f - propia | comunes
         0-6|m - psalter
         """
-        return "WERSET"
+        psalter_str = str(self.calendar.current_psalter_week)
+        day_str = str(self.calendar.lg_day.weekday())
+
+        werse = self.base[psalter_str][day_str].get('werse', "")
+        if werse != "":
+            return f"\u2731 WERSET \u2731\n{werse}"
+        else:
+            return "\n*** no werse for today in database ***\n"
 
     def readings(self):
         """
-        1 lecture + own responsory
-        from propia de  tempore (pdt)
-
-        s|f - propia|comunes
-
-        2 lecture + own responsory
-        from date
-
-        else from 'pdt'
-
+        1 lecture + own responsory -> propia de tempore (pdt) | when s|f -> propia|comunes
+        2 lecture + own responsory -> date | -> 'pdt'
         """
-        return "CZYTANIA"
+        psalter_str = str(self.calendar.current_psalter_week)
+        day_str = str(self.calendar.lg_day.weekday())
 
-    def tedeum(self, clasic_type=True, full=True):
+        lectures = ''
+        for r in ["i_reading", "ii_reading"]:
+            lectures += self.base[psalter_str][day_str].get(r, f"*** no {r} for today in database ***") + "\n"
+
+        return lectures
+
+    def tedeum(self):
         poetic = self.const["lec_tedeum_poetic"]
         poetic_extra = self.const["lec_tedeum_poetic_extra"]
         clasic = self.const["lec_tedeum_clasic"]
         clasic_extra = self.const["lec_tedeum_clasic_extra"]
 
-        if full:
+        if self.full:
             clasic += clasic_extra
             poetic += poetic_extra
 
-        if self.rank in ["f", "s"]:
-            return clasic if clasic_type else poetic
+        if self.rank in ["f", "s"] and not self.is_lent:
+            return clasic if self.clasic_td else poetic
         else:
             return ""
 
     def prayer(self):
-        """
-        pdt|propia|comunes
-
-        """
+        """ pdt|propia|comunes """
         ...
-        prayer = ""
-
-        return f"MODLITWA\nMódlmy się:\n{prayer}\nAmen." if self.joined else ""
+        psalter_str = str(self.calendar.current_psalter_week)
+        day_str = str(self.calendar.lg_day.weekday())
+        prayer = self.base[psalter_str][day_str].get("prayer", f"*** no prayers for today in database ***")
+        return f"MODLITWA\nMódlmy się:\n{prayer}\nAmen.\n" if not self.joined else ""
 
     def dismisal(self):
-        return "K. B\u0142ogos\u0142awmy Panu.\nW.  Bogu niech b\u0119d\u0105 dzi\u0119ki.\n" if self.joined else ""
+        return "K. B\u0142ogos\u0142awmy Panu.\nW.  Bogu niech b\u0119d\u0105 dzi\u0119ki.\n" if not self.joined else ""
 
 
 class Base:
@@ -209,7 +293,10 @@ def save_const(placement, const_txt):
 
 if __name__ == "__main__":
     x = Readings()
-    x.rank = "m"
-    x.is_lent = True
+    x.is_lent = False
     x.joined = False
+    x.clasic_td = True
+    x.full = True
+
     x.pray()
+
